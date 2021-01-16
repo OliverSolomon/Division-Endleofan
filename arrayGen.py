84dbde0126f67ff490f182f23226d22ab5a5e3fc
OliverSolomon <oliversolomonwainaina@gmail.com>
2021-01-16 23:40:42 +0300

    small change to the description

--- arrayGen.py
+++ arrayGen.py
@@ -1,3 +1,4 @@
+"""Generates a random array of numbers of range -100000 to 100000 with a sure number divisible by 11. It is to be inherited by main.py but can be modified as a standalone"""
 from random import randint
 
 arrayA = []
